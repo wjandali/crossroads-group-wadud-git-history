7499e1631c579a8b7977ea7e2c28bada2d353918
Wadud Jandali <wjandali@Waduds-MacBook-Pro.local>
2019-12-17 14:50:31 -0800

    Retrieve commits POC

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {
+  Button,
+  Container,
+  Row
+} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import GitHub from 'github-api';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+
+    const gh = new GitHub({
+      username: 'wjandali',
+      token: '088227f747cfb91f744baa3e56df843070f007b5'
+    });
+
+    this.state = {
+      commits: [],
+      repository: (gh.getRepo('wjandali', 'crossroads-group-wadud-git-history'))
+    }
+  }
+
+  componentDidMount() {
+    this.state.repository.listCommits((commits) => {
+      window.x = arguments;
+      console.log(arguments);
+      console.log(commits);
+    }).then((commits) => {
+      console.log('commits:');
+      console.log(commits);
+    });
+  }
+  
+  render() {
+    return (
+      <Container>
+        <div className="App">
+          <header className="App-header">
+              <Button variant="primary">Primary</Button>
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </Container>
+    );
+  }
 }
 
 export default App;
