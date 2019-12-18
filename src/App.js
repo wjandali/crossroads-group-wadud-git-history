7cdc79cce47207e617383474513329ca93f8af53
Wadud Jandali <wjandali@Waduds-MacBook-Pro.local>
2019-12-17 17:13:25 -0800

    Infinite scroll added, but buggy, WIP

--- src/App.js
+++ src/App.js
@@ -2,60 +2,106 @@ import React, { Component } from 'react';
 import {
   Button,
   Container,
+  Navbar,
   Row
 } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import moment from 'moment';
+
+import InfiniteScroll from 'react-infinite-scroller';
 import GitHub from 'github-api';
 
 import './App.css';
 
+const TIME_WINDOW = 2;
+
 class App extends Component {
+
   constructor() {
     super()
 
+    // use the current time as a reference
+    const now = moment();
+
+    // for infinite function with the Github API (which has until/since options)
+    // this is strictly because I wanted to tack on infinite scroll and multiple requests
+    // otherwise I'd just fetch and display in one go
+    const since = moment(now).subtract(TIME_WINDOW, 'hours');
+    const until = moment(now);
+    const requestsMade = 0;
+
     const gh = new GitHub({
       username: 'wjandali',
-      token: '088227f747cfb91f744baa3e56df843070f007b5'
     });
 
     this.state = {
+      requestsMade,
+      since,
+      until,
       commits: [],
-      repository: (gh.getRepo('wjandali', 'crossroads-group-wadud-git-history'))
+      total: null,
+      repository: gh.getRepo('wjandali', 'crossroads-group-wadud-git-history')
     }
   }
 
   componentDidMount() {
-    this.state.repository.listCommits((commits) => {
-      window.x = arguments;
-      console.log(arguments);
-      console.log(commits);
-    }).then((commits) => {
-      console.log('commits:');
-      console.log(commits);
+    this.fetchTotal();
+    this.fetchCommits();
+  }
+
+  fetchTotal() {
+    this.state.repository.listCommits().then((response) => {
+      this.setState({...this.state, total: response.data.length});
     });
+  }
+
+  fetchCommits(count) {
+    let { since, until } = this.state; 
+
+    // replace until with the previous timestamp so that we fetch the next batch a time slice back
+    until = moment(since);
+    since.subtract(TIME_WINDOW, 'hours');
+
+    const requestsMade = this.state.requestsMade + 1;
+
+    this.state.repository.listCommits({since, until}).then((response) => {
+      this.setState(
+        {
+          ...this.state,
+          requestsMade,
+          commits: this.state.commits.concat(response.data),
+          until,
+          since
+        }
+      );
+    });
+  }
+
+  renderCommit(commit) {
+    const { sha, commit: { author, message } } = commit;
+
+    return (
+      <div key={sha}>
+        {sha}
+      </div>
+    );
+
   }
   
   render() {
     return (
-      <Container>
-        <div className="App">
-          <header className="App-header">
-              <Button variant="primary">Primary</Button>
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
-      </Container>
+      <div style={{height: '700px', overflow: 'auto'}}>
+        <Container fluid={true}>
+          <InfiniteScroll
+            loadMore={this.fetchCommits.bind(this)}
+            hasMore={this.state.requestsMade < 5 || !this.state.total || this.state.commits.length >= this.state.total}
+            loader={<div className="loader" key={0}>Loading ...</div>}
+          >
+            { this.state.commits.map((commit) => this.renderCommit(commit)) }
+          </InfiniteScroll>
+        </Container>
+      </div>
     );
   }
 }
